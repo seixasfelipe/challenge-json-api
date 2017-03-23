7e830966016c4fd8d4d93b103f05a2c02d5b4f1c
Felipe Seixas <seixasfelipe@gmail.com>
2017-03-22 23:39:04 -0400

    simple crawler (need parallel workers)

--- crawler.js
+++ crawler.js
@@ -1,5 +1,7 @@
 var http = require('http'),
-    hostname
+    hostname,
+    hashQueue = []
+    secret = ''
 
 function httpRequest(params, callback) {
     var options = {
@@ -14,9 +16,11 @@ function httpRequest(params, callback) {
         }
     }
 
+    console.log(`=> new request ${options.path}`)
+
     var req = http.request(options, (res) => {
         res.on('data', (chunk) => {
-            console.log(`BODY: ${chunk}`)
+            console.log(`response: ${chunk}`)
             if(callback) {
                 callback(chunk)
             }
@@ -30,12 +34,31 @@ function httpRequest(params, callback) {
     req.end();
 }
 
+function processHash(json, session) {
+
+    if (json.secret) secret += json.secret
+    if (typeof json.next === 'object') hashQueue = hashQueue.concat(json.next)
+    if (typeof json.next === 'string') hashQueue.push(json.next)
+
+    var hash = hashQueue.splice(0, 1)
+
+    if (hash.length === 0) console.log(`secret is: ${secret}`)
+
+    httpRequest({ path: '/' + hash[0], session: session }, function(res) {
+        var json = JSON.parse(res)
+        processHash(json, session)
+    })
+}
+
 function getSession() {
 
     httpRequest({ path: '/get-session' }, function(res) {
-        console.log('callback ' + res)
-        var json = JSON.parse(res)
-        console.log(json.session)
+        var json = JSON.parse(res), session = json.session
+
+        httpRequest({ path: '/start', session: session }, function(res) {
+            var json = JSON.parse(res)
+            processHash(json, session)
+        })
     })
 }
 
